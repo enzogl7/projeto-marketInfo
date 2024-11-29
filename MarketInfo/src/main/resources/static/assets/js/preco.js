25318600fdacf36ae4de1fa558c8f1aebea362b3
Enzo Lima <enzolima527@gmail.com>
2024-11-29 20:58:08 -0300

    .

--- MarketInfo/src/main/resources/static/assets/js/preco.js
+++ MarketInfo/src/main/resources/static/assets/js/preco.js
@@ -1,6 +1,38 @@
+// INICIO ORDENACAO TABELA
+// FIM ORDENACAO TABELA
+
+
+// INICIO FILTRO/PESQUISA TABELA
+document.getElementById("pesquisaNomePreco").addEventListener("keyup", filtrarTabelaPrecos);
+document.getElementById("pesquisaPreco").addEventListener("keyup", filtrarTabelaPrecos);
+
+function filtrarTabelaPrecos() {
+    var nomeFilter = document.getElementById("pesquisaNomePreco").value.toLowerCase();
+    var precoFilter = document.getElementById("pesquisaPreco").value.toLowerCase();
+    var table = document.querySelector(".table");
+    var rows = table.querySelectorAll("tbody tr");
+
+    rows.forEach(function (row) {
+        var nome = row.querySelector("td:nth-child(1)").textContent.toLowerCase();
+        var marca = row.querySelector("td:nth-child(2)").textContent.toLowerCase();
+
+        var nomeMatch = nome.indexOf(nomeFilter) > -1 || nomeFilter === "";
+        var marcaMatch = marca.indexOf(precoFilter) > -1 || precoFilter === "";
+
+        if (nomeMatch && marcaMatch) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+    });
+}
+// FIM FILTRO/PESQUISA TABELA
+
+//FORMATAÇÃO DO INPUT DE PREÇO
 document.addEventListener('DOMContentLoaded', function () {
     applyCleave('#preco');
     applyCleave('#precoAtual');
+    applyCleave('#pesquisaPreco')
 });
 
 function applyCleave(inputSelector) {
@@ -32,7 +64,16 @@ document.getElementById('editarPrecoForm').addEventListener('submit', function(e
     precoInputEdicao.value = precoValorEdicao;
 });
 
+document.getElementById('pesquisaPreco').addEventListener('submit', function(event) {
+    var precoInputPesquisa = document.getElementById('pesquisaPreco');
+    var precoValorPesquisa = precoInputPesquisa.value;
 
+    precoValorPesquisa = precoValorPesquisa.replace('R$', '').replace(/\D/g, '');
+    precoInputPesquisa.value = precoValorPesquisa;
+});
+// FIM FORMATAÇÃO DE PREÇO
+
+// INICIO MODAIS
 function modalEditarPreco(button) {
     var idPrecoEdicao = button.getAttribute('data-id');
     $('#modalEditarPreco').modal('show');
@@ -51,3 +92,5 @@ function modalExcluirPreco(button) {
     $('#modalExcluirPreco').modal('show');
     document.getElementById('idPrecoExclusao').value = idPrecoExclusao;
 }
+//FIM MODAIS
+
