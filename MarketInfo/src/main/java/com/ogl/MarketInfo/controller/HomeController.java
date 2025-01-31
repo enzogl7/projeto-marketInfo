9f3a647904e0abc349f2a2bcfacf517067c96ff1
Enzo Lima <enzolima527@gmail.com>
2025-01-31 15:03:20 -0300

    [main]

--- MarketInfo/src/main/java/com/ogl/MarketInfo/controller/HomeController.java
+++ MarketInfo/src/main/java/com/ogl/MarketInfo/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.ogl.MarketInfo.controller;
 
+import com.ogl.MarketInfo.model.Usuario;
 import com.ogl.MarketInfo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home(Model model) {
+        model.addAttribute("usuario", usuarioService.getUsuarioLogado());
         model.addAttribute("roleLogada", usuarioService.getRolesUsuarioLogado());
         return "/home/home";
     }
